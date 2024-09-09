24857ab02bc71197621767034dd9b91047f66982
mmartofel <mmartofe@redhat.com>
2024-09-09 19:23:25 +0200

    refactor files naming and service port

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 
+oc delete application rhel9-server -n openshift-gitops
 
-oc create -f ./argocd/vms-app.yaml
-
+oc create -f ./argocd/rhel9-server-app.yaml
 
 oc extract secrets/openshift-gitops-cluster -n openshift-gitops --keys=admin.password --to -
 
