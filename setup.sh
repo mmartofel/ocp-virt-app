a57473d5afba47e97c29a9db54cb608e2dbc7cf5
mmartofel <mmartofe@redhat.com>
2024-09-13 01:29:15 +0200

    finalize frontend integration

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 
 oc delete application rhel-servers-playground -n openshift-gitops
 oc delete application quarkus-api -n openshift-gitops
+oc delete application nodejs-frontend -n openshift-gitops
 
 oc create -f ./argocd/rhel9-server-app.yaml
 oc create -f ./argocd/api-app.yaml
+oc create -f ./argocd/frontend-app.yaml
 
+echo "Argo admin password is: "
 oc extract secrets/openshift-gitops-cluster -n openshift-gitops --keys=admin.password --to -
 
 
