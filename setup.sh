e3634dbfa7dfe66e25b5b0ec00b2e207b6950c69
mmartofel <mmartofe@redhat.com>
2024-09-13 01:39:53 +0200

    files naming refactoring

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 
-oc delete application rhel-servers-playground -n openshift-gitops
+oc delete application virtual-machines -n openshift-gitops
 oc delete application quarkus-api -n openshift-gitops
 oc delete application nodejs-frontend -n openshift-gitops
 
-oc create -f ./argocd/rhel9-server-app.yaml
-oc create -f ./argocd/api-app.yaml
-oc create -f ./argocd/frontend-app.yaml
+oc create -f ./argocd/virtual-machines-app.yaml
+oc create -f ./argocd/quarkus-api-app.yaml
+oc create -f ./argocd/nodejs-frontend-app.yaml
 
 echo "Argo admin password is: "
 oc extract secrets/openshift-gitops-cluster -n openshift-gitops --keys=admin.password --to -
