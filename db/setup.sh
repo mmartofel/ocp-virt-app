d2f997298306396bdfc439a737dafe6df115741c
mmartofel <mmartofe@redhat.com>
2024-09-12 17:54:58 +0200

    setup postgres database at centos9-server vm part 3

--- db/setup.sh
+++ db/setup.sh
@@ -1,2 +1,3 @@
-# sudo -u postgres psql -c 'create database demo'
 sudo -u postgres psql < /tmp/ocp-virt-app/db/user-demo.sql
+sudo -u postgres echo "host   all             all             0.0.0.0/0      md5" >> /var/lib/pgsql/data/pg_hba.conf
+sudo systemctl restart postgresql
