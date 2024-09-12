f08152d10451cbb71b51f7588021c249bbe1aa91
mmartofel <mmartofe@redhat.com>
2024-09-12 19:52:18 +0200

    add quarkus api to connect to vm based postgres, fix 1

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 
 oc delete application rhel-servers-playground -n openshift-gitops
+oc delete application quarkus-api -n openshift-gitops
 
 oc create -f ./argocd/rhel9-server-app.yaml
+oc create -f ./argocd/api-app.yaml
 
 oc extract secrets/openshift-gitops-cluster -n openshift-gitops --keys=admin.password --to -
 
